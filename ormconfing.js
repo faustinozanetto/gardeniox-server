00470e1bb48f30f81a224e8d39e072fd412180dc
Faustino Zanetto <faustizanetto@gmail.com>
2021-02-14 21:55:23 -0300

    Fix

--- ormconfing.js
+++ ormconfing.js
@@ -1,15 +1,8 @@
 const type = process.env.TYPEORM_TYPE || 'postgres';
-const username = process.env.TYPEORM_USERNAME || 'user';
-const password = process.env.TYPEORM_PASSWORD || 'pass';
-const host = process.env.TYPEORM_HOST || 'db';
-const port = parseInt(process.env.TYPEORM_PORT, 10) || 5432;
-const database = process.env.TYPEORM_DATABASE || 'data';
 
 module.exports = {
   type,
-  url:
-    process.env.DATABASE_URL ||
-    `${type}://${username}:${password}@${host}:${port}/${database}`,
+  url: process.env.DATABASE_URL,
   entities: [
     process.env.NODE_ENV === 'test'
       ? 'src/entities/**/*.entity.ts'
